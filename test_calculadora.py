b2b018233c8cb12a6d63ad54811716f69e582e2a
Vitor Oikawa <vitor9998@gmail.com>
2021-03-19 14:59:52 -0300

    Unittest - Parte1

---
+++ test_calculadora.py
@@ -0,0 +1,31 @@
+import unittest
+from calculadora import soma
+
+class TestCalculadora(unittest.TestCase): #Tem comecar com Test
+    def test_soma_5_e_5_deve_retornar_10(self): #Método tem que comecar com test também e deve ser bem descritivo
+        self.assertEqual(soma(5, 5), 10)
+
+    def test_soma_5_negativo_e_5_deve_retonar_0(self):
+        self.assertEqual(soma(-5, 5), 0)
+
+    def test_soma_varias_entradas(self):
+        x_y_saidas = (
+            (10, 10, 20),
+            (5, 5, 10),
+            (1.5, 1.5, 3),
+            (-5, 5, 0),
+            (100, 100, 200),
+        )
+
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(soma(x, y), saida)
+
+    
+
+
+unittest.main(verbosity=2)
+
+
+
